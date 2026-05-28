1c2f15a5e7decc8cc39887606bb4707a28db4545
Almedin Agushi <77640112+Almedin-Agushi@users.noreply.github.com>
2026-05-28 14:07:13 +0200

    Fix duplicate stock column migration

--- database/migrations/2026_05_19_104311_add_stock_to_products_table.php
+++ database/migrations/2026_05_19_104311_add_stock_to_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
 {
     Schema::table('products', function (Blueprint $table) {
 
-        $table->integer('stock')->default(1);
+        // $table->integer('stock')->default(1);
 
     });
 }
